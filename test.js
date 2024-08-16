c4f62a7053b5f6ad5f37caa5dab14f0e6db5f6ea
yashwanth reddy <yashwanthappidi@gmail.com>
2024-08-16 21:32:49 +0530

    second

--- test.js
+++ test.js
@@ -5,9 +5,9 @@ const client = require('twilio')(accountSid, authToken);
 
 client.messages
     .create({
-        body: 'HI WELCOME TO TWILLO',
+        body: 'HI swetha how are you this is your brother from godavarikhani it is just a text sample',
         from: '+12514283831',
-        to: '+918688009078'
+        to: '+919652201241'
     })
     .then(message => console.log(message.sid))
     
